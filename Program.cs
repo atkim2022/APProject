a8e4af2fc22cc98d49fa31d1454be0793d558730
atkim2022 <atkim2022@chadwickschool.org>
2022-03-16 13:01:28 -0700

    IMPLEMENT

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq; 
+
+namespace APProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is the first number you would like to sum?");
+            string FirstString = Console.ReadLine();
+            int FirstNum = int.Parse(FirstString);
+            
+            List<int> values = new List<int>(); 
+
+            
+        }
+
+        public static int SumAll(List<int> values)
+        {
+            return 2; 
+        }
+
+
+    }
+}
